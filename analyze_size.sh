29a88340fab8e31c97b2b0eaaa7ec758c2f6308b
saun-kun <saunkinv7@gmail.com>
2024-10-30 16:05:55 +0300

    1 commit

---
+++ analyze_size.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+analyze_size() {
+    local path="$1"
+    local size=$(du -hs "$path" 2>/dev/null | cut -f1)
+    echo $size
+}
+
+items=$(ls -A)
+
+result=()
+for item in $items; do
+    size=$(analyze_size "$item")
+    result+=("$size $item")
+done
+
+printf "%s\n" "${result[@]}" | sort -rh | column -t
